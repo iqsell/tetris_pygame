2630789d3816406b3850eadc301dd0896d795745
iqsell <gwelki89@gmail.com>
2023-12-22 13:02:18 +0300

    first version of tetris on pygame. preview, tetris, gameover and restart game.

---
+++ main.py
@@ -0,0 +1,63 @@
+from preview import Preview
+from settings import *
+from sys import exit
+
+# components
+from game import Game
+from score import Score
+from random import choice
+
+
+class Main:
+    def __init__(self):
+
+        # general
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption('Tetris')
+
+        # shapes
+        self.next_shapes = [choice(list(TETROMINOS.keys())) for shape in range(3)]
+
+        # components
+        self.game = Game(self.get_next_shape, self.update_score)
+        self.score = Score()
+        self.preview = Preview()
+
+    def update_score(self, lines, score, level):
+        self.score.lines = lines
+        self.score.score = score
+        self.score.level = level
+
+    def get_next_shape(self):
+        next_shape = self.next_shapes.pop(0)
+        self.next_shapes.append(choice(list(TETROMINOS.keys())))
+        return next_shape
+
+    def run(self):
+        running = True
+        self.preview.start_screen()
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+                    # отображение
+            self.display_surface.fill(GRAY)
+
+            # компоненты
+
+            self.game.run()
+            self.score.run()
+
+            # обновление экрана
+            pygame.display.update()
+            self.clock.tick()
+        pygame.quit()
+        exit()
+
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
---
+++ preview.py
@@ -0,0 +1,43 @@
+# need realize bd, scores, records, requests scores, register
+from game import *
+from settings import *
+
+
+class Preview:
+
+    def __init__(self):
+        self.surface = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.rect = self.surface.get_rect(bottomright=(WINDOW_WIDTH - PADDING, WINDOW_HEIGHT - PADDING))
+        self.display_surface = pygame.display.get_surface()
+
+    def load_image(self, name, colorkey=None):
+        fullname = os.path.join('data', name)
+        # если файл не существует, то выходим
+        if not os.path.isfile(fullname):
+            print(f"Файл с изображением '{fullname}' не найден")
+            sys.exit()
+        image = pygame.image.load(fullname)
+        if colorkey is not None:
+            image = image.convert()
+            if colorkey == -1:
+                colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey)
+        else:
+            image = image.convert_alpha()
+        return image
+
+    def start_screen(self):
+
+        img_path = os.path.join('data', 'images.jpeg')
+        gameover_img = pygame.image.load(img_path)
+        gameover_img = pygame.transform.scale(gameover_img, (700, 850))
+        self.display_surface.blit(gameover_img, (0, 0))
+        pygame.display.flip()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                    return
+            pygame.display.flip()  # Обновление экрана после отрисовки
